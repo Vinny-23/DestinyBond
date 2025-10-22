d79798cf1dceb61065d844c412a77942e6e3378f
Vinny-23 <alvingore70@gmail.com>
2025-10-22 23:06:58 +0200

    Added Color

--- App.tsx
+++ App.tsx
@@ -283,7 +283,7 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#ffffffff',
+    color: '#000000ff',
     marginBottom: 20,
   },
   input: {
